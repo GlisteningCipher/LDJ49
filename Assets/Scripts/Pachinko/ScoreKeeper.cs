89eaf1c4b07d792ff448bf90c24622a53cb7c97c
James Takeshi Iwamasa <james.takeshi.iwamasa@gmail.com>
2021-10-01 23:35:52 -0700

    Added basic scoring system

---
+++ Assets/Scripts/Pachinko/ScoreKeeper.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages score (i.e. number of balls) in pachinko game.
+/// </summary>
+public class ScoreKeeper : MonoBehaviour
+{
+    public static ScoreKeeper instance = null;
+    int currScore = 0; // Current score (i.e. # of balls)
+    public int CurrScore
+    {
+        get => currScore;
+        set
+        {
+            currScore = value;
+        }
+    }
+    public TMPro.TextMeshProUGUI ScoreText;
+
+    /// <summary>
+    /// Calculate score when ball hits bucket, and destroys ball.
+    /// UnityEvent callback.
+    /// </summary>
+    public void Score(Bucket bucket, Ball ball)
+    {
+        CurrScore += bucket.BucketMultipler * ball.BallScore;
+        Destroy(ball.gameObject);
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        ScoreText.text = CurrScore.ToString();
+    }
+}
