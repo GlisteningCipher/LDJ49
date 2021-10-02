bd6b05529e3fe161d9abe882f866dd829e23d5d6
GlisteningCipher <jlawhome08@gmail.com>
2021-10-02 12:09:25 -0700

    main menu window prefabs

---
+++ Assets/Scripts/Menu/Window.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class Window : MonoBehaviour
+{
+    const float TWEEN_DURATION = 0.1f;
+    [SerializeField] bool openOnAwake = false;
+
+    void Awake()
+    {
+        if (openOnAwake) transform.localScale = Vector3.one;
+        else transform.localScale = new Vector3(1, 0, 1);
+    }
+
+    void Open()
+    {
+        transform.DOScaleY(1f, TWEEN_DURATION);
+    }
+
+    void Close()
+    {
+        transform.DOScaleY(0f, TWEEN_DURATION);
+    }
+}
