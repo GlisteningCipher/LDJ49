c726493d5377295043e16dd4aa71b6d0c4caa3d3
GlisteningCipher <jlawhome08@gmail.com>
2021-10-04 01:01:20 -0700

    game unover if last ball scores

--- Assets/BallChecker.cs
+++ Assets/BallChecker.cs
@@ -4,14 +4,10 @@ public class BallChecker : MonoBehaviour
 {
     [SerializeField] ScoreKeeper scoreKeeper;
 
-    void Start()
-    {
-        transform.localScale = new Vector3(1, 0, 1);
-    }
-
     void Update()
     {
         if (scoreKeeper.CurrScore <= 0) transform.localScale = Vector3.one;
+        else transform.localScale = new Vector3(1, 0, 1);
     }
 
 
