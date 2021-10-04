516386756a645f94ba8014f3eca4c8d059adacc9
GlisteningCipher <jlawhome08@gmail.com>
2021-10-03 21:01:01 -0700

    level picker

--- Assets/Scripts/GameManagerSO.cs
+++ Assets/Scripts/GameManagerSO.cs
@@ -4,9 +4,19 @@ using UnityEngine.SceneManagement;
 [CreateAssetMenu(fileName = "Game Manager", menuName = "Managers/Game Manager", order = 1)]
 public class GameManagerSO : ScriptableObject
 {
-    public void Play()
+    public void PlayEasy()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayMedium()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayHard()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void Exit()
